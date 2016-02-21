06b5a1d9b51454c60028bbd7f95809398df99691
Jou <j_jounou94@hotmail.com>
2016-02-21 05:31:21 +0100

    que ja estigui siusplau

--- app/src/main/java/com/example/marlen/hack_upc/MongoUPC.java
+++ app/src/main/java/com/example/marlen/hack_upc/MongoUPC.java
@@ -7,16 +7,18 @@ package com.example.marlen.hack_upc;
 import com.loopj.android.http.*;
 
 public class MongoUPC {
-    private static final String BASE_URL = "https://api.mongolab.com/api/1/databases/hackupc/collections/?apiKey=t6Jm8gibg6IEHUJ9u7mlKQAv_lvVfm-c";
+    private static final String BASE_URL = "https://api.mongolab.com/api/1/databases/hackupc/collections/";
     private static final String API_KEY = "t6Jm8gibg6IEHUJ9u7mlKQAv_lvVfm-c";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        params.put("apiKey", API_KEY);
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        params.put("apiKey", API_KEY);
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
--- app/src/main/java/com/example/marlen/hack_upc/MongoUPCUsage.java
+++ app/src/main/java/com/example/marlen/hack_upc/MongoUPCUsage.java
@@ -1,6 +1,7 @@
 package com.example.marlen.hack_upc;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,13 +13,17 @@ import java.util.ArrayList;
  * Created by Jou on 20/2/2016.
  */
 public class MongoUPCUsage {
-    public void getServicio(String type,int MaxDist, final ArrayList<Service> s) throws JSONException {
-        MongoUPC.get("", null, new JsonHttpResponseHandler() {
+    public void getServicio(String type,double lon, double lat, final ArrayList<Service> s) throws JSONException {
+        RequestParams params = new RequestParams();
+        params.put("q","{\"Type\" : \"" + type + "\", \"Coordenadas\" :" +
+                " {$near:{$geometry:{type:\"Point\", coordinates:[" + lon + ", " + lat + "]}}}}");
+        params.put("c","0");
+        MongoUPC.get("", params, new JsonHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONArray response) {
                 super.onSuccess(statusCode, headers, response);
-                for (int i = 0; i < response.length();i++) {
+                for (int i = 0; i < response.length(); i++) {
                     JSONObject jsonobject = null;
                     try {
                         jsonobject = (JSONObject) response.get(i);
@@ -26,12 +31,12 @@ public class MongoUPCUsage {
                         e.printStackTrace();
                         //System.out.println("FUCK YOU!");
                     }
-                    Service serv=new Service();
-                    serv.setName (jsonobject.optString("Name"));
-                    serv.setType (jsonobject.optString("Type"));
-                    serv.setCity (jsonobject.optString("Address"));
-                    serv.setMaxDist (jsonobject.optInt("Dist"));
-                    serv.setTelNum (jsonobject.optInt("Contact"));
+                    Service serv = new Service();
+                    serv.setName(jsonobject.optString("Name"));
+                    serv.setType(jsonobject.optString("Type"));
+                    serv.setCity(jsonobject.optString("Address"));
+                    serv.setMaxDist(jsonobject.optInt("Dist"));
+                    serv.setTelNum(jsonobject.optInt("Contact"));
                     s.add(serv);
                 }
 
